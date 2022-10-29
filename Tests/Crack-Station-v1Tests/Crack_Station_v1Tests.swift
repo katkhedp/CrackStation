40d2abc522e237b7cc3ea7b9e5edc3cf4f44a5e8
katkhedp <katkhedp@oregonstate.edu>
2022-10-29 04:32:43 +0000

    Added Test File

--- Tests/Crack-Station-v1Tests/Crack_Station_v1Tests.swift
+++ Tests/Crack-Station-v1Tests/Crack_Station_v1Tests.swift
@@ -3,9 +3,8 @@ import XCTest
 
 final class Crack_Station_v1Tests: XCTestCase {
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Crack_Station_v1().text, "Hello, World!")
+        let crack_station = Crack_Station_v1()
+        let lookupTable = try crack_station.loadDictionaryFromDisk()
+        
     }
 }
