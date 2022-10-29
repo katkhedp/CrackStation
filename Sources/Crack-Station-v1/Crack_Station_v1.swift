99663ba013c37aa8048069f91faa99e5bbdf331d
katkhedp <katkhedp@oregonstate.edu>
2022-10-29 03:46:02 +0000

    Initialised Swift Package

---
+++ Sources/Crack-Station-v1/Crack_Station_v1.swift
@@ -0,0 +1,6 @@
+public struct Crack_Station_v1 {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/Crack-Station-v1Tests/Crack_Station_v1Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Crack_Station_v1
+
+final class Crack_Station_v1Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Crack_Station_v1().text, "Hello, World!")
+    }
+}
