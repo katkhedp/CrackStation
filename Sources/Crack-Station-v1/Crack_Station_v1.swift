03f74bbdf78cbe1811c7a86c92e627e2bc4f6697
katkhedp <katkhedp@oregonstate.edu>
2022-10-29 04:08:24 +0000

    Added Crack-Station code

--- Sources/Crack-Station-v1/Crack_Station_v1.swift
+++ Sources/Crack-Station-v1/Crack_Station_v1.swift
@@ -3,4 +3,16 @@ public struct Crack_Station_v1 {
 
     public init() {
     }
+     static func loadDictionaryFromDisk() throws -> [String : String] {
+        guard let path = Bundle.module.url(forResource: "data", withExtension: "json") else { return [:] }
+
+        let data = try Data(contentsOf: path)
+        let jsonResult = try JSONSerialization.jsonObject(with: data)
+
+        if let lookupTable: Dictionary = jsonResult as? Dictionary<String, String> {
+            return lookupTable
+        } else {
+            return [:]
+        }
+    }
 }
