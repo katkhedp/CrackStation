e9b91ee8d88f3fe5fd69377368f0f1d35ceebadf
katkhedp <katkhedp@oregonstate.edu>
2022-11-20 15:44:38 -0800

    Added mvp's three placed hashes

--- Sources/CrackStation/data-hard.py
+++ Sources/CrackStation/data-hard.py
@@ -6,7 +6,7 @@ data_dict = {}
 iter_1 =0
 iter_2 =0
 
-alpaNums=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9']
+alpaNums=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','?','!']
 
 for x in alpaNums:
     hash = hashlib.sha1(x.encode())
@@ -19,8 +19,15 @@ for iter_2 in alpaNums:
         hash = hashlib.sha1(var.encode()) 
         res = hash.hexdigest()
         data_dict[res] = var
-    
 
+for iter_3 in alpaNums:
+    for iter_4 in alpaNums:
+        for iter_5 in alpaNums:
+            var= iter_3+''+iter_4+''+iter_5   
+            hash = hashlib.sha1(var.encode()) 
+            res = hash.hexdigest()
+            data_dict[res] = var
+        
 
 with open('data.json', 'w') as file_json:
     json.dump(data_dict, file_json)
