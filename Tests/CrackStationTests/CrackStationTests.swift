ef365e957850051cb2aa80404ec04a9df88b0a44
katkhedp <katkhedp@oregonstate.edu>
2022-11-20 19:27:54 -0800

    Updated test File

--- Tests/CrackStationTests/CrackStationTests.swift
+++ Tests/CrackStationTests/CrackStationTests.swift
@@ -2,9 +2,10 @@ import XCTest
 @testable import CrackStation
 
 final class CrackStationTests: XCTestCase {
-    func testExample() throws {
-        let crack_station = CrackStation()
-        let lookupTable = try crack_station.loadDictionaryFromDisk()
-        
+    func testMVPsha1_3() throws {
+
+        let inputhash = "e1e154e5261dc0011fd40e84d2cad1566e9ccfe3"
+        let decrypted = CrackStation().decrypt(shaHash: inputhash) ?? nil
+        XCTAssertEqual(decrypted, "a!A")
     }
 }
